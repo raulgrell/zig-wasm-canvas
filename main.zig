cdc328978ef3d47e57605a7d2bc66ccb5cdf96b3
Tythos <bekirkpatrick@proton.me>
2024-10-04 22:10:55 -0700

    pulled javascript into es6 module; fstreamlined html; added styling; started to scale dimensions

--- main.zig
+++ main.zig
@@ -53,7 +53,7 @@ var offsetUniformLocation: c_int = undefined;
 var positionBuffer: c_uint = undefined;
 
 export fn onInit() void {
-    glClearColor(0.1, 0.1, 0.1, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
